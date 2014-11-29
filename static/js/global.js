1327c16a8c78416794c9d4b780e7c086069bed2a
Elise R <elise@wiseeddy.com>
2014-11-29 23:48:45 +0000

    switching from jquery to d3

--- static/js/global.js
+++ static/js/global.js
@@ -1,99 +1,64 @@
-;(function($, window) {
-
+;(function(window) {
     "use strict";
-
     var document = window.document;
-
-    $(document).ready(function() {
-
+	document.addEventListener("DOMContentLoaded",function() {
         var all_files = [],
             current_file_id = 0,
             locked = false,
             prev_count_files = 0,
             waiting = 0,
-            drop, dropzone, handleNextFile, handleReaderLoad, noopHandler;
-
-        noopHandler = function(evt) {
-            evt.stopPropagation();
-            evt.preventDefault();
-        };
-
+            drop, dropzone, handleNextFile, handleReaderLoad,
+        noopHandler = function(evt) {evt.stopPropagation();evt.preventDefault();};
         drop = function(evt) {
-
             noopHandler(evt);
-
             var files = evt.dataTransfer.files,
                 count = files.length,
                 i, j;
-
-            if ( count > 0 ) {
-
+            if (count>0) {
                 prev_count_files = all_files.length;
-
-                if ( $("#dropzoneLabel").length !== 0 ) {
-                    $("#dropzone").html('');
+                if (d3.select("#dropzone")[0].length!=0) {d3.select("#dropzone").html('')}
+                for (i = prev_count_files + waiting, j = 0; i < prev_count_files + files.length + waiting;i++,j++) {
+                   var fbox = d3.select("#dropzone").append('div').attr("class","file " + "f"+i); 
+  				   fbox.append("div").attr("class","name").text(files[j].name)
+                   fbox.append("div").attr("class","progress").text("Waiting...")
                 }
-
-                for ( i = prev_count_files + waiting, j = 0; i < prev_count_files + files.length + waiting; i++, j++ ) {
-                    $("#dropzone").append('<div class="file ' + i + '"><div class="name">' + files[j].name + '</div><div class="progress">Waiting...</div></div>');
-                }
-
                 waiting += count;
-
-                if ( ! locked ) {
+                if (!locked) {
                     waiting -= count;
                     all_files.push.apply(all_files, files);
                     handleNextFile();
                 }
             }
         };
-
         handleReaderLoad = function(evt) {
-
-            var current_file = {};
-
-            current_file.name = all_files[current_file_id].name;
-            current_file.type = all_files[current_file_id].type;
-            current_file.contents = evt.target.result;
-
-            $.post('/upload', JSON.stringify(current_file), function(data, textStatus, jqXHR) {
-
-                if ( jqXHR.status === 200 ) {
-                    $(".file." + current_file_id + " .progress").html("Uploaded");
+            var current_file = {name:all_files[current_file_id].name,type:all_files[current_file_id].type,contents:evt.target.result};
+            d3.xhr('/upload').post(JSON.stringify(current_file), function(error,XHR) {
+               if (XHR.status === 200) {
+                    d3.select(".file." + "f"+current_file_id + " .progress").html("Uploaded"); 
                 } else {
-                    $(".file." + current_file_id + " .progress").html("Failed");
+                    d3.select(".file." + "f"+current_file_id + " .progress").html("Failed");
                 }
-
                 all_files[current_file_id] = 1;
                 current_file_id++;
                 handleNextFile();
             });
         };
-
         handleNextFile = function() {
-
-            if ( current_file_id < all_files.length ) {
-
+            if (current_file_id < all_files.length) {
                 locked = true;
-
-                $(".file." + current_file_id + " .progress").html("Uploading...");
-
+                d3.select(".file." + "f"+current_file_id + " .progress").html("Uploading...");
                 var current_file = all_files[current_file_id],
                     reader = new FileReader();
-
                 reader.onload = handleReaderLoad;
                 reader.readAsDataURL(current_file);
-
             } else {
                 locked = false;
             }
         };
-
         dropzone = document.getElementById("dropzone");
         dropzone.addEventListener("dragenter", noopHandler, false);
         dropzone.addEventListener("dragexit", noopHandler, false);
         dropzone.addEventListener("dragover", noopHandler, false);
         dropzone.addEventListener("drop", drop, false);
     });
-
-}(jQuery, window));
+}(window));
